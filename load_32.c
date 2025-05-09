e3e3264211d917163d94c5eb02811353e7888bde
0xSayf <dindinferda16@gmail.com>
2025-05-09 22:25:23 +0100

    S

--- load_32.c
+++ load_32.c
@@ -3,10 +3,15 @@
 void    ft_load_32(char *ptr)
 {
     DWORD elfanew;
+    DWORD size_of_headers;
     DWORD size_of_image;
     char *image_base;
 
     elfanew = *(DWORD *)(ptr + 0x3C);
     size_of_image = *(WORD *)(ptr + elfanew + 0x50);
     image_base = VirtualAlloc(NULL,size_of_image,0x00002000 | 0x00001000, 0x40);
+    if(!image_base)
+        exit(1);
+    size_of_headers = *(DWORD *)(ptr + elfanew + 0x54);
+    memcpy(image_base, ptr , size_of_headers);
 }
