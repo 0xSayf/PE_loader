77a2e46f6eec43af03af1df8a2cc3a86be6aa4ec
0xSayf <dindinferda16@gmail.com>
2025-05-09 20:13:13 +0100

    SA

--- load_headers.c
+++ load_headers.c
@@ -2,5 +2,11 @@
 
 void    ft_load_32(char *ptr)
 {
-    
+    DWORD elfanew;
+    DWORD size_of_image;
+    char *image_base;
+
+    elfanew = *(DWORD *)(ptr + 0x3C);
+    size_of_image = *(WORD *)(ptr + elfanew + 0x50);
+    image_base = VirtualAlloc(NULL,size_of_image,0x00002000 | 0x00001000, 0x40);
 }
