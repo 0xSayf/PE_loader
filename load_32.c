bc1c6923b4357ddc0001ee97335bc4d898983f9f
0xSayf <dindinferda16@gmail.com>
2025-05-10 14:25:48 +0100

    SA

--- load_32.c
+++ load_32.c
@@ -5,14 +5,16 @@ void    ft_load_32(char *ptr)
     DWORD elfanew;
     DWORD size_of_headers;
     DWORD size_of_image;
+    DWORD entrypoint;
     WORD  num_of_sections;
     unsigned int section_RVA;
     char *sections_back;
     char *image_base;
 
     elfanew = *(DWORD *)(ptr + 0x3C);
-    size_of_image = *(WORD *)(ptr + elfanew + 0x50);
+    size_of_image = *(DWORD *)(ptr + elfanew + 0x50);
     size_of_headers = *(DWORD *)(ptr + elfanew + 0x54);
+    entrypoint = *(DWORD*)(ptr + elfanew + 0x28);
     num_of_sections = *(WORD *)(ptr + elfanew + 0x6);
     image_base = VirtualAlloc(NULL,size_of_image, 0x00002000 | 0x00001000, 0x40);
     if(!image_base)
@@ -22,10 +24,11 @@ void    ft_load_32(char *ptr)
     int i = 0;
     while (i < num_of_sections)
     {
-        sections_back = image_base + (section_RVA + 12);
-        memcpy(sections_back, ptr + section_RVA, section_RVA + 8);
+        sections_back = image_base + *(DWORD*)(section_RVA + 12);
+        memcpy(sections_back, ptr + section_RVA + 0x14 , section_RVA + 0x10);
         section_RVA += 40;
         i++;
     }
-    ((void (*)(void)) image_base + (ptr + ))();
+    void (*entry)() = (void (*)())(image_base + entrypoint);
+    entry();
 }
